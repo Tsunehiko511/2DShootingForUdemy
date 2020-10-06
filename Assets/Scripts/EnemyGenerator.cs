930521aed8882151581f701aaab88741904cfb75
嶋津恒彦 <simanezumi1989@gmail.com>
2020-10-06 13:48:54 +0900

    敵の攻撃実装前のプロジェクト
    ・敵の攻撃実装前のプロジェクト

---
+++ Assets/Scripts/EnemyGenerator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyGenerator : MonoBehaviour
+{
+    public GameObject EnemyPrefab;
+
+    private int random;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Spawn", 2f, 1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Spawn()
+    {
+        Vector3 spawnPositon = new Vector3(
+            Random.Range(-2.9f,2.9f),
+            transform.position.y,
+            transform.position.z);
+
+        Instantiate(EnemyPrefab, spawnPositon, transform.rotation);
+    }
+}
---
+++ Assets/Scripts/EnemyShip.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShip : MonoBehaviour
+{
+    //爆破エフェクト
+    public GameObject explosion;
+
+    //GameControllerのAddScoreメソッドを使用するため入れ物を用意
+    GameController gameController;
+
+
+    float offset;
+
+    void Start()
+    {
+        //揺れ方をランダムにする
+        offset = Random.Range(0, 2f * Mathf.PI);
+
+        //GameObject.Find("")でカッコ内のオブジェクトを取得し、GetComponentでそのオブジェクトの指定した部品を取得してくる
+        gameController = GameObject.Find("GameController").GetComponent<GameController>();
+
+
+    }
+
+
+    //接触判定があった場合(collisonには接触したオブジェクトの情報が入っている)
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        //playerと敵が接触した時
+        if (collision.CompareTag("Player") == true)
+        {
+            //破壊する時に爆破エフェクト生成（生成したいもの、場所、回転）
+            Instantiate(explosion, collision.transform.position, transform.rotation);
+            gameController.GameOver();
+        }
+        //Bulletと敵が接触した時
+        else if (collision.CompareTag("Bullet") == true)
+        {
+            //スコア加算
+            gameController.AddScore();
+        }
+
+        //破壊する時に爆破エフェクト生成（生成したいもの、場所、回転）
+        Instantiate(explosion, transform.position, transform.rotation);
+        //enemyの機体を破壊
+        Destroy(gameObject);
+        //collisionはぶつかった相手の情報が入っている。この場合は弾
+        Destroy(collision.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //左右に揺れながら、下に移動する＊＊＊＊＊＊＊＊＊＊＊＊＊今回修正
+        transform.position -= new Vector3(
+            Mathf.Cos(Time.frameCount * 0.05f + offset) * 0.01f,
+            Time.deltaTime,
+            0);
+
+        //撃った弾のy軸が-3以下の位置にいったら破壊
+        if (transform.position.y < -3)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
